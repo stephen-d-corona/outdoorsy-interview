edcada5dd978d66ef3325805dcba358d180264b4
Personal <personal@Stephens-MBP.attlocal.net>
2023-08-11 13:36:20 -0500

    useRentals is fetching data. not hooked up to handle changes to options yet. lets build the user facing components next. but first, lunch.

---
+++ app/constants.ts
@@ -0,0 +1,2 @@
+// in a production application, these would be env variables 
+export const baseUrl = "https://search.outdoorsy.com"
---
+++ app/useRentals.ts
@@ -0,0 +1,56 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { baseUrl } from "./constants"
+import { Rental } from "./types"
+
+export const useRentals = ({ keywords, limit, offset }:
+    { keywords: string, limit: number, offset: number }
+) => {
+    const [error, setError] = useState()
+    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState<Rental[]>([])
+
+    useEffect(() => {
+        setLoading(true)
+        setData([])
+        axios.get<ResponseDto>(`${baseUrl}/rentals`, {
+            params: {
+                'filter[keywords]': keywords,
+                'page[limit]': limit,
+                'page[offset]': offset
+            }
+        })
+            .then((response) => {
+                console.log("my response!", response)
+
+                // map response to a Rental
+                const rentals = response.data.data.map((d) => ({
+                    id: d.id,
+                    name: d.attributes.name,
+                    imageUrl: d.attributes.primary_image_url
+                }))
+                console.log("my datas!", rentals)
+                setData(rentals)
+            })
+            .catch(setError)
+            .finally(() => {
+                setLoading(false)
+            })
+    }, [])
+
+    return {
+        data, loading, error
+    }
+}
+
+interface ResponseDto {
+    data: RentalDto[]
+}
+
+interface RentalDto {
+    id: string,
+    attributes: {
+        name: string,
+        primary_image_url: string
+    }
+}
